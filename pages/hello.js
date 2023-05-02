8a884fada0199613d15b3463112e6da345b1b29b
zahitrios <zahit@xenotech.mx>
2023-05-02 10:47:55 -0600

    Arrow function removed

--- pages/hello.js
+++ pages/hello.js
@@ -1,11 +1,9 @@
 import styles from '@/styles/Hello.module.css'
 
-const hello = () => {
+export default function Hello() {
     return (
         <div className={[styles.div]}>
             <h1>Hello hello</h1>
         </div>
     )
 }
-
-export default hello
